d7200fdc3dbc89f822e105cb7c29d8571c5cc2d6
Odohi David <odohi.david@gmail.com>
2021-09-22 11:06:29 +0100

    week 5

--- week5/tcp.go
+++ week5/tcp.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	// 65535
+	// 127.0.0.1
+	// 192.168.0.1
+	// 255.255.255.255
+	service := "localhost:1200" // localhost:1200, 127.0.0.1, 0.0.0.0
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	checkError(err)
+
+	fmt.Printf("listening on %v:%v\n", tcpAddr.IP, tcpAddr.Port)
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+
+	for {
+		conn, err := listener.Accept() // waits for someone to connect
+		if err != nil {
+			continue
+		}
+		fmt.Println("someone connected")
+		daytime := time.Now().String()
+		conn.Write([]byte(daytime)) // don't care about return value
+		conn.Close()                // we're finished with this client
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
--- week5/tcpclient.go
+++ week5/tcpclient.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s host:port ", os.Args[0])
+		os.Exit(1)
+	}
+	service := os.Args[1]
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+
+	_, err = conn.Write([]byte("HEAD / HTTP/1.0\r\n\r\n"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+
+	//result, err := readFully(conn)
+	result, err := ioutil.ReadAll(conn)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println(string(result))
+
+	os.Exit(0)
+}
